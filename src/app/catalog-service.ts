fb70d619b48a789de752d6d9a5d9480aa0c2e85b
astragirl2425 <huongdtd234111e@st.uel.edu.vn>
2026-01-17 19:50:42 +0700

    Done Catalog Ex14

--- src/app/catalog-service.ts
+++ src/app/catalog-service.ts
@@ -6,19 +6,19 @@ import { Injectable } from '@angular/core';
 export class CatalogService {
   datas = [
     {
-      "Cateid": "cate1", "CateName": "nuoc ngot",
+      "Cateid": "cate1", "CateName": "NƯỚC NGỌT",
       "Products": [
-        { "ProductId": "p1", "ProductName": "Coca", "Price": 100, "Image": "assets/h1.png" },
-        { "ProductId": "p2", "ProductName": "Pepsi", "Price": 300, "Image": "assets/h2.png" },
-        { "ProductId": "p3", "ProductName": "Sting", "Price": 200, "Image": "assets/h3.png" },
+        { "ProductId": "p1", "ProductName": "Coca", "Price": 100.000, "Image": "assets/coca.jpeg" },
+        { "ProductId": "p2", "ProductName": "Pepsi", "Price": 300.000, "Image": "assets/pepsi.jpg" },
+        { "ProductId": "p3", "ProductName": "Sting", "Price": 200.000, "Image": "assets/sting.jpg" },
       ]
     },
     {
-      "Cateid": "cate2", "CateName": "Bia",
+      "Cateid": "cate2", "CateName": "BIA",
       "Products": [
-        { "ProductId": "p4", "ProductName": "Heleiken", "Price": 500, "Image": "assets/h4.png" },
-        { "ProductId": "p5", "ProductName": "333", "Price": 400, "Image": "assets/h5.png" },
-        { "ProductId": "p6", "ProductName": "Sai Gon", "Price": 600, "Image": "assets/h6.png" },
+        { "ProductId": "p4", "ProductName": "Heineken", "Price": 500.000, "Image": "assets/heineken.jpg" },
+        { "ProductId": "p5", "ProductName": "333", "Price": 400.000, "Image": "assets/333.jpg" },
+        { "ProductId": "p6", "ProductName": "Sài Gòn", "Price": 600.000, "Image": "assets/saigon.jpg" },
       ]
     },
   ];
